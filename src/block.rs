0bee65abafc60368ef82b1384a00bb8db4d4ff1a
naveed949 <naveediqbal949@gmail.com>
2024-06-01 20:03:03 +0500

    chore: Add Cargo.toml and Block struct implementation

---
+++ src/block.rs
@@ -0,0 +1,62 @@
+use crate::transaction::Transaction;
+use crate::pow::ProofOfWork;
+use crate::utils;
+use serde::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Block {
+    timestamp: i64,
+    pre_block_hash: String,
+    hash: String,
+    transactions: Vec<Transaction>,
+    nonce: i64,
+    height: usize,
+}
+
+impl Block {
+    pub fn new_block(pre_block_hash: String, transactions: &[Transaction], height: usize) -> Block {
+        let mut block = Block {
+            timestamp: utils::current_timestamp(),
+            pre_block_hash,
+            hash: String::new(),
+            transactions: transactions.to_vec(),
+            nonce: 0,
+            height,
+        };
+        let pow = ProofOfWork::new_proof_of_work(block.clone());
+        let (nonce, hash) = pow.run();
+        block.nonce = nonce;
+        block.hash = hash;
+        return block;
+    }
+
+    pub fn get_transactions(&self) -> &[Transaction] {
+        self.transactions.as_slice()
+    }
+    pub fn get_pre_block_hash(&self) -> String {
+        self.pre_block_hash.clone()
+    }
+    pub fn get_hash(&self) -> &str {
+        self.hash.as_str()
+    }
+    pub fn get_hash_bytes(&self) -> Vec<u8> {
+        self.hash.as_bytes().to_vec()
+    }
+    pub fn get_timestamp(&self) -> i64 {
+        self.timestamp
+    }
+    pub fn get_height(&self) -> usize {
+        self.height
+    }
+    pub fn hash_transactions(&self) -> Vec<u8> {
+        let mut txhashs = vec![];
+        for transaction in &self.transactions {
+            txhashs.extend(transaction.get_id());
+        }
+        utils::sha256_digest(txhashs.as_slice())
+    }
+    pub fn generate_genesis_block(transaction: &Transaction) -> Block {
+        let transactions = vec![transaction.clone()];
+        return Block::new_block(String::from("None"), &transactions, 0);
+    }
+}
