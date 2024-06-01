28bd7823fbe3ab0ca391717bc5e4f93ef6b11cc5
naveed949 <naveediqbal949@gmail.com>
2024-06-01 20:03:31 +0500

    chore: Add block, pow, transaction, and utils modules

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+pub mod block;
+pub mod pow;
+pub mod transaction;
+pub mod utils;
---
+++ src/utils.rs
@@ -0,0 +1,17 @@
+
+use std::time::{SystemTime, UNIX_EPOCH};
+use sha2::Sha256;
+
+pub fn current_timestamp() -> u64 {
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    since_the_epoch.as_secs()
+}
+
+pub fn sha256_digest(data: &[u8]) -> Vec<u8> {
+    let mut hasher = Sha256::new();
+    hasher.update(data);
+    hasher.finalize().to_vec()
+}
